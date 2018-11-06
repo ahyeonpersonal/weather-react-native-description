7c21e4b1f88662445c3cb21aca6ded65588944c6
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-07 11:40:43 +1300

    Create App.js

---
+++ Day1/App.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native'; //1. import ActivityIndicator : loading GIF
+
+export default class App extends React.Component {
+
+  /* 2. If is is loaded it's gonna show weather, If not it's gonna show loading view */
+  
+  state={
+    isLoaded : false
+  }
+
+  render() {
+
+    const {isLoaded} = this.state;
+
+    return (
+      <View style={styles.container}>
+      {/* 3. Is it loaded? 
+             True => we don't have anything to show yet so 'null'
+             False => show <view>
+      */}
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText>Getting Weather</Text> </View>}
+          </View> )
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#fff',
+  },
+  //4. Creating 'loading' ,'loadingText'
+  loading: {
+    backgroundColor:'#FDF6AA',
+    justifyContent :"flex-end",
+    paddingLeft:25 
+  },
+  loadingText:{
+    fontSize:38,
+    marginBottom: 100
+  }
+});
