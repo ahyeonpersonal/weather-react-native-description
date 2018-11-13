462ed3da1e09c6874fea1f0dfddbcd4756627019
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-11-13 17:25:55 +1300

    Update App.js

--- Day8/App.js
+++ Day8/App.js
@@ -1,140 +1,98 @@
-import React, {Component} from 'react'; 
-import {StyleSheet, Text, View} from 'react-native';
-import {LinearGradient} from "expo";
-//13-1. import ion icons : expo.github.io/vector
-import {Ionicons} from "@expo/vector-icons";
+import * as React from 'react';
+import { Text, View, StyleSheet, ActivityIndicator, StatusBar } from 'react-native'; //1. import ActivityIndicator : loading GIF, //12-1. import StatusBar
+import Weather from "./Weather"; //6. import Weather.js
 
 
-//38. Create weatherCases
-const weatherCases = {
-    Rain :{
-        colors:["#00C6FB", "#005BEA"], //Gradient color
-        title:"Raining",
-        subtitle : "For more info look outside",
-        icon : 'ios-rainy'
-    },
-    Clear :{
-        colors:["#00C6FB", "#005BEA"],
-        title:"Sunny",
-        subtitle :" Let's get tanned",
-        icon : 'ios-sunny'
-    },
-    Thunderstorm:{
-        colors:["#00C6FB", "#005BEA"],
-        title:"Thunderstorm",
-        subtitle:"OMG, It's raining hard with thunder",
-        icon : 'ios-thunderstorm'
-    },
-    Clouds:{
-        colors:["#00C6FB", "#005BEA"],
-        title:"Clouds",
-        subtitle : "Typical Auckland...",
-        icon : 'ios-cloudy'
-    },
-    Snow:{
-        colors:["#00C6FB", "#005BEA"],
-        title:"Snowy",
-        subtitle : "Let's build a snow man",
-        icon :'ios-snow'
-    },
-    Drizzle:{
-        colors:["#00C6FB", "#005BEA"],
-        title:"Drizzle",
-        subtitle : "Kinda foggy?!",
-        icon : 'ios-rainy-outline'
-    }
-}
+export default class App extends React.Component {
 
+  /* 2. If is is loaded it's gonna show weather, If not it's gonna show loading view */
+  state={
+    isLoaded : false //8.to load 'Weather.js'
+    //15. false
+    //15. isLoaded : Ture = Displaying Weather.js (weather information)
+    //15. isLoaded : false = Displaying loading page
+    //Once it gets the weather information =>  it set it as True and disply the weather information
+    ,
+    error:null //20. add error:null in state
+};
+  
+  //16. Get current position geolocation
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position =>{
+        //17. once it gets the current location information, is Loaded : true = means, it will displaying Weather.js 
+        this.setState({
+          isLoaded: true
+          
+        }); 
+        //console.log(position);
+      },
+      //18. If there is error, console log the 'error'
+      
+      error =>{
+        //19. delete console.log(error), add 'error' in state
+        //console.log(error);
 
-//33. Refactoring :change the code structure
-// Now we have a data in our state,
-// make component as stateless component : not gonna hold state, not gonna make it from class instead get it from function
-// Delete Weather.js #11 ~ #34
-//export default class Weather extends Component {
-//  render(){
-//    return(
-//    <LinearGradient colors={["#00C6FB", "#005BEA"]} style={style.container} >
-//     {/*//9. Create Weather structure*/}
-     
-//     {/*//10. Upper - Icon , Temperature*/}
-//     <View style={style.upper}> 
-//     {/* 13-2: import Ionicons*/}
-//      <Ionicons color="white" size={144} name="ios-rainy" /> 
-//      //<Text>Icon here!</Text>
-//      <Text style={style.temp}>Temperature here</Text>
-//     </View>
-//
-//     {/*//11. Lower - Title , Subtitle */} 
-//     <View style={style.lower}>
-//      <Text style={style.title}>Raining like S**T</Text>
-//      <Text style={style.subtitle}>For more info look outside</Text>
-//     </View>
-//      
-//    </LinearGradient>
-//    );
-//  }
-//}
-
-// 34. Refactoring start
-function Weather({temp}){ //37. Add {temp} and replace 35 to {temp} 
-    return( //39.Update icon, colors, title, subtitle
-        <LinearGradient colors={weatherCases["Clear"].colors} style={style.container} >
-            <View style={styles.upper}>
-                <Ionicons color="white" size={144} name={weatherCases["Clear"].icon} />
-                <Text style={styles.temp}>{temp}</Text> 
-            </View>
-            
-            <View style={styles.lower}>
-                <Text style={styles.title}>{weatherCases["Clear"].title}</Text> 
-                <Text style={styles.subtitle}>{weatherCases["Clear"].subtitle}/Text>
-            </View>
-        </LinearGradient> 
+        this.setState({
+         //21. add Error
+         //error:error
+        //24.set error as "Something went wrong" to check errorText
+          error:"Oops, something went wrong"
+        })
+        
+      }
     );
+  }
+
+  /*
+  16. Original version would be like....
+  navigator.geolocation.getCurrentPosition(function(position){console.log(position)})
+   */
+
+  render() {
+
+    const {isLoaded, error} = this.state; //22. add Error
+
+    return (
+      <View style={styles.container}>
+      {/* 3. Is it loaded? 
+      True => we don't have anything to show yet so 'null'
+      False => show <view>
+      */}
+      {/* 7. import <Weather /> when it's loaded, to do that change state as 'true' as well*/}
+        <StatusBar hidden={true} /> {/* 12-2. import status Bar*/}
+        //36. Givg <Weather /> temperature information & //37.Change F to C
+        {isLoaded ? <Weather temp={Math.floor(temperature - 273.15)} /> : (  
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting weather</Text>
+            //23. If there is an error, i will display {error}, otherwise, null
+            {error ? <Text style={styles.errorText}>{error}</Text> : null} 
+          </View>  
+        )}
+      </View>  
+    ); 
+  }
 }
 
-export default Weather;
-//34. Refactoring end
-
-//35. There are 3 things we need to change
-//1. Icon
-//2. Temperature
-//3. Title & Subtitle
-//means we need 'props'
-
-const style= StyleSheet.create({
-  container:{
-    flex:1
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#fff',
   },
-  upper:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    //14. change backgroundColor to transparent => icon is able to see
-    backgroundColor:"transparent"
-  },
-  temp:{
-    fontSize:48,
+  //24. adding Error style
+  errorText:{
+    color:"red",
     backgroundColor:"transparent",
-    color:'white',
-    marginTop:"10"
+    marginBottom:40
   },
-  lower:{
-    flex:1,
-    alignItems:"flex-start",
-    justifyContent:"flex-end",
+  //4. Creating 'loading' ,'loadingText'
+  loading: {
+    backgroundColor:'#FDF6AA',
+    justifyContent :"flex-end",
     paddingLeft:25
   },
-  title:{
+  loadingText:{
     fontSize:38,
-    backgroundColor:"transparent",
-    color:'white',
-    marginBottom :10,
-    fontWeight:"300"
-  },
-  subtitle:{
-    fontSize:24,
-    backgroundColor:"transparent",
-    color:'white',
-    marginBottom:24
+    marginBottom: 24
   }
-})
+});
